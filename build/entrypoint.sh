61b40bb98cf4e2ac47c94105c5568be37696c504
Karsten Hassel <hassel@gmx.de>
2018-09-27 00:34:10 +0200

    update entrypoint.sh, Download of personal config/modules

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -8,4 +8,16 @@ if [ ! -f /opt/magic_mirror/config ]; then
     cp -Rn /opt/magic_mirror/unmount_config/. /opt/magic_mirror/config
 fi
 
+if [ -n "$2" ]; then
+    # $2 contains url-path to file magic_mirror.tar.gz
+	# e.g. https://my-website.com/mm-stuff
+	rm -rf /opt/magic_mirror/config/*
+	rm -rf /opt/magic_mirror/modules/*
+	cd /opt
+	curl -O $2/magic_mirror.tar.gz
+	tar -zxvf magic_mirror.tar.gz
+fi
+
+cd /opt/magic_mirror
+
 $1
