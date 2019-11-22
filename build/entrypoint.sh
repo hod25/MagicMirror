6af142fa50e2107fac268b03b13fc4e96846d111
Karsten Hassel <hassel@gmx.de>
2019-11-22 22:07:57 +0100

    refactor mounts and entrypoint

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,29 +1,13 @@
 #!/bin/sh
 
 if [ ! -f /opt/magic_mirror/modules/default/defaultmodules.js ]; then
-  cp -Rn /opt/magic_mirror/unmount_modules/. /opt/magic_mirror/modules
+  cp -rn /opt/magic_mirror/mount_ori/modules/. /opt/magic_mirror/modules
   chown -R node:node /opt/magic_mirror/modules/
 fi
 
 if [ ! -f /opt/magic_mirror/config/config.js ]; then
-  cp -Rn /opt/magic_mirror/unmount_config/. /opt/magic_mirror/config
+  cp -rn /opt/magic_mirror/mount_ori/config/. /opt/magic_mirror/config
   chown -R node:node /opt/magic_mirror/config/
 fi
 
-if [ -n "$2" ]; then
-  # $2 contains url-path to file magic_mirror.tar.gz
-  # e.g. https://my-website.com/mm-stuff
-  rm -rf /opt/magic_mirror/config/*
-  rm -rf /opt/magic_mirror/modules/*
-  cd /opt
-  curl -O $2/magic_mirror.tar.gz
-  tar -zxvf magic_mirror.tar.gz
-fi
-
-chown -R node:node /home/node/
-
-su - node
-
-cd /opt/magic_mirror
-
-exec $1
+exec su - node -c "cd /opt/magic_mirror && $1 $2"
--- build/slim-entrypoint.sh
+++ build/slim-entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-if [ ! -f /opt/magic_mirror/modules ]; then
-  cp -Rn /opt/magic_mirror/unmount_modules/. /opt/magic_mirror/modules
+mkdir -p /opt/magic_mirror/modules
+mkdir -p /opt/magic_mirror/config
+
+if [ ! -f /opt/magic_mirror/modules/default/defaultmodules.js ]; then
+  cp -rn /opt/magic_mirror/mount_ori/modules/. /opt/magic_mirror/modules
   chown -R node:node /opt/magic_mirror/modules/
 fi
 
-if [ ! -f /opt/magic_mirror/config ]; then
-  cp -Rn /opt/magic_mirror/unmount_config/. /opt/magic_mirror/config
+if [ ! -f /opt/magic_mirror/config/config.js ]; then
+  cp -rn /opt/magic_mirror/mount_ori/config/. /opt/magic_mirror/config
   chown -R node:node /opt/magic_mirror/config/
 fi
 
@@ -16,7 +19,7 @@ su -c "cd /opt/magic_mirror/modules/default && npm install || true" - node
 
 if [ "$BuildEnv"=="rpi" ]; then
   su -c "cd /opt/magic_mirror && npm start || true" - node
-  su -c "arp-scan localhost || true" - node  
+  su -c "arp-scan localhost || true" - node
 fi
 
 su -c "cd /opt/magic_mirror && node serveronly" - node
