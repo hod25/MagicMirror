0583ee282595788abab44960bce743fbb455a333
Karsten Hassel <hassel@gmx.de>
2020-01-06 20:55:39 +0100

    added test, one entrypoint.sh, variables for spectron and electron

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir -p /opt/magic_mirror/modules
+mkdir -p /opt/magic_mirror/config
+
 if [ ! -f /opt/magic_mirror/modules/default/defaultmodules.js ]; then
   cp -rn /opt/magic_mirror/mount_ori/modules/. /opt/magic_mirror/modules
   chown -R node:node /opt/magic_mirror/modules/
@@ -10,8 +13,33 @@ if [ ! -f /opt/magic_mirror/config/config.js ]; then
   chown -R node:node /opt/magic_mirror/config/
 fi
 
-su - node
+if [ "$StartEnv" = "test" ]; then 
 
-cd /opt/magic_mirror
+  echo "start tests"
+  su -c "/docker-entrypoint.sh" - node
+  su -c "cd /opt/magic_mirror && grunt" - node
+  su -c "cd /opt/magic_mirror && npm run test:unit" - node
+  su -c "cd /opt/magic_mirror && npm run test:e2e" - node
 
-exec $1 $2
+elif [ "$StartEnv" = "slim" ]; then 
+  
+  echo "start slim entrypoint"
+  rm -rf /opt/magic_mirror/magicmirror/ && cd /opt/magic_mirror && git clone https://gitlab.com/khassel/magicmirror.git
+
+  su -c "cd /opt/magic_mirror/modules/default && npm install || true" - node
+
+  if [ "$BuildEnv" = "rpi" ]; then
+    su -c "cd /opt/magic_mirror && npm start || true" - node
+    su -c "arp-scan localhost || true" - node
+  fi
+
+  su -c "cd /opt/magic_mirror && node serveronly" - node
+else
+
+  echo "start magicmirror"
+  su - node
+
+  cd /opt/magic_mirror
+
+  exec $1 $2
+fi
