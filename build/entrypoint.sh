da4246c2eddeb355c4678b113b57151e25f1dd69
Karsten Hassel <hassel@gmx.de>
2020-01-17 22:13:04 +0100

    option to use latest dependencies in package.json, chown removed from entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,12 +5,10 @@ mkdir -p /opt/magic_mirror/config
 
 if [ ! -f /opt/magic_mirror/modules/default/defaultmodules.js ]; then
   cp -rn /opt/magic_mirror/mount_ori/modules/. /opt/magic_mirror/modules
-  chown -R node:node /opt/magic_mirror/modules/
 fi
 
 if [ ! -f /opt/magic_mirror/config/config.js ]; then
   cp -rn /opt/magic_mirror/mount_ori/config/. /opt/magic_mirror/config
-  chown -R node:node /opt/magic_mirror/config/
 fi
 
 if [ "$StartEnv" = "test" ]; then 
