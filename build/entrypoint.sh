5b30a2b3391529f3283c83ddac0b7d0f447d02c0
Karsten Hassel <hassel@gmx.de>
2020-01-21 20:48:16 +0100

    use sudo chmod in entrypoint.sh instead extra permission container, optimize slim entrypoint

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -11,6 +11,9 @@ if [ ! -f /opt/magic_mirror/config/config.js ]; then
   cp -rn /opt/magic_mirror/mount_ori/config/. /opt/magic_mirror/config
 fi
 
+sudo chown -R node:node /opt/magic_mirror/modules
+sudo chown -R node:node /opt/magic_mirror/config
+
 if [ "$StartEnv" = "test" ]; then 
 
   echo "start tests"
@@ -29,21 +32,32 @@ elif [ "$StartEnv" = "slim" ]; then
   
   echo "start slim entrypoint"
 
-  rm -rf /opt/magic_mirror/magicmirror/ 
-  cd /opt/magic_mirror 
+  rm -rf /opt/magic_mirror/magicmirror/
+  cd /opt/magic_mirror
+
+  echo git clone
   git clone https://gitlab.com/khassel/magicmirror.git
- 
-  cd /opt/magic_mirror/modules/default
+
+  echo npm install
+  cd /opt/magic_mirror/vendor
   npm install || true
 
   cd /opt/magic_mirror
 
   if [ "$BuildEnv" = "rpi" ]; then
+    echo arp-scan
+    sudo arp-scan localhost &
+    echo npm start
     npm start || true
-    arp-scan localhost || true
+
+  else
+
+    echo node serveronly
+    node serveronly || true
   fi
 
-  node serveronly
+  sudo chown -R node:node /home/node
+  sudo chown -R node:node /opt/magic_mirror
 
 else
 
