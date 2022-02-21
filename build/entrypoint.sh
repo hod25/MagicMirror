04ccdb5642390d034ff2a8197cde61ac17d1fa53
Karsten Hassel <hassel@gmx.de>
2022-02-21 19:51:43 +0100

    fix entrypoint.sh again, move envsubst after chown is done

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -6,7 +6,7 @@ css_dir="/opt/magic_mirror/css"
 
 [ ! -d "${default_dir}" ] && MM_OVERRIDE_DEFAULT_MODULES=true
 
-if [ "${MM_OVERRIDE_DEFAULT_MODULES}" = "true" ]; then 
+if [ "${MM_OVERRIDE_DEFAULT_MODULES}" = "true" ]; then
   echo "copy default modules to host ..."
   sudo rm -rf ${default_dir}
   sudo mkdir -p ${default_dir}
@@ -14,18 +14,6 @@ if [ "${MM_OVERRIDE_DEFAULT_MODULES}" = "true" ]; then
 fi
 
 sudo mkdir -p ${config_dir}
-
-if [ -f "${config_dir}/config.js.template" ]; then
-  if [ -f "${config_dir}/config.js" ]; then
-    sudo mv -v ${config_dir}/config.js ${config_dir}/config.js-old
-  fi
-  sudo -E envsubst < ${config_dir}/config.js.template > sudo tee ${config_dir}/config.js > /dev/null
-fi
-
-if [ ! -f "${config_dir}/config.js" ]; then
-  echo "copy default config.js to host ..."
-  sudo cp /opt/magic_mirror/mount_ori/config/config.js.sample ${config_dir}/config.js
-fi
 
 sudo mkdir -p ${css_dir}
 
@@ -39,14 +27,24 @@ fi
 # create css/custom.css file https://github.com/MichMich/MagicMirror/issues/1977
 [ ! -f "${css_dir}/custom.css" ] && sudo touch ${css_dir}/custom.css
 
-if [ ! -z "$(id | grep node)" ]; then
-  echo "chown modules and config folder ..."
-  sudo chown -R node:node /opt/magic_mirror/modules
-  sudo chown -R node:node ${config_dir}
-  sudo chown -R node:node ${css_dir}
+echo "chown modules and config folder ..."
+sudo chown -R node:node /opt/magic_mirror/modules &
+sudo chown -R node:node ${config_dir}
+sudo chown -R node:node ${css_dir}
+
+if [ -f "${config_dir}/config.js.template" ]; then
+  if [ -f "${config_dir}/config.js" ]; then
+    mv -v ${config_dir}/config.js ${config_dir}/config.js-old
+  fi
+  envsubst < ${config_dir}/config.js.template > ${config_dir}/config.js
 fi
 
-if [ "$MM_SHOW_CURSOR" = "true" ]; then 
+if [ ! -f "${config_dir}/config.js" ]; then
+  echo "copy default config.js to host ..."
+  cp /opt/magic_mirror/mount_ori/config/config.js.sample ${config_dir}/config.js
+fi
+
+if [ "$MM_SHOW_CURSOR" = "true" ]; then
   echo "enable mouse cursor ..."
   sed -i "s|  cursor: .*;|  cursor: auto;|" /opt/magic_mirror/css/main.css
 fi
@@ -54,7 +52,7 @@ fi
 [ -z "$TZ" ] && export TZ="$(wget -qO - http://geoip.ubuntu.com/lookup | sed -n -e 's/.*<TimeZone>\(.*\)<\/TimeZone>.*/\1/p')"
 
 if [ -z "$TZ" ]; then
-  echo "***WARNING*** could not set timezone, please set TZ variable in docker-compose.yml, see https://gitlab.com/khassel/magicmirror#timezone" 
+  echo "***WARNING*** could not set timezone, please set TZ variable in docker-compose.yml, see https://gitlab.com/khassel/magicmirror#timezone"
 else
   echo "timezone is $TZ"
   sudo ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
