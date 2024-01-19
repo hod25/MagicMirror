354d3626618319700c57793ee26fd57e615d2c98
Karsten Hassel <hassel@gmx.de>
2024-01-20 00:09:23 +0100

    entrypoint.sh: if no command is provided use defaults depending on whether electron is installed

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -89,5 +89,14 @@ else
     . "./$_script"
   fi
 
-  exec env TZ=$TZ "$@"
+  if [ $# -eq 0 ]; then
+    # if no params are provided, add defaults depending if electron is installed:
+    if command -v node_modules/.bin/electron &> /dev/null; then
+      exec env TZ=$TZ npm start
+    else
+      exec env TZ=$TZ npm run server
+    fi
+  else
+    exec env TZ=$TZ "$@"
+  fi
 fi
